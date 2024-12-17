5ef0450b4f4bd54ffdcf6002c078c0d59a306d7c
Ussene Carlos <159960079+ussenematato@users.noreply.github.com>
2024-12-18 00:43:04 +0200

    Getting selected currencies (updated every 30 seconds)
    Returns selected currencies (updated every 30 seconds)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Conversor de Moedas
  Um conversor de moedas com actualização min a min
+
+ # Bibliotecas usadas (instaladas atraves do pip)
+ -> customtkinter - para a tela
+ instrução no terminal: pip install customtkinter
+
+ -> xmltodict - para converter e manipular arquivos xml em dicionários
+  instrução no terminal: pip install xmltodict
+  
+ -> requests - para requesição em tempo real das cotações das moedas
+   instrução no terminal: pip install requests
+
+ # Links para arquivos xml e cotações
+ -> https://economia.awesomeapi.com.br/xml/available
+ -> https://economia.awesomeapi.com.br/xml/available/uniq
+ -> https://docs.awesomeapi.com.br/api-de-moedas
--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import customtkinter
 from pegar_moedas import nomes_moedas, conversoes_disponiveis
+from pegar_cotacao import pegar_cotacao_moeda
 
 # criar e configurara a janela
 customtkinter.set_appearance_mode("dark")
@@ -31,13 +32,19 @@ campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_convers
 campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["Selecione uma moeda de origem"])
 
 def converter_moeda():
-    print("Converter moeda")
+    # print("Converter moeda")
+    moeda_origem = campo_moeda_origem.get()
+    moeda_destino = campo_moeda_destino.get()
+    if moeda_origem and moeda_destino:
+        cotacao = pegar_cotacao_moeda(moeda_origem, moeda_destino)
+        texto_cotacao_moeda.configure(text=f"1 {moeda_origem} = {cotacao} {moeda_destino}")
     
 
 botao_converter = customtkinter.CTkButton(janela, text="Converter", command=converter_moeda)
 
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
 
+texto_cotacao_moeda = customtkinter.CTkLabel(janela, text="")
 
 moedas_disponiveis = nomes_moedas()
 for codigo_moeda in moedas_disponiveis:
@@ -53,6 +60,7 @@ campo_moeda_origem.pack(padx=10)
 texto_moeda_destino.pack(padx=10, pady=10)
 campo_moeda_destino.pack(padx=10)
 botao_converter.pack(padx=10, pady=10)
+texto_cotacao_moeda.pack(padx=10, pady=10)
 lista_moedas.pack(padx=10, pady=10)
 
 # rodar a janela
